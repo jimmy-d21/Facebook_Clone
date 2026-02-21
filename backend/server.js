e723f063e9175096ee11f3ce12b70ebf2b3b389b
bernadith <bernadith0110@gmail.com>
2026-02-21 13:52:28 +0800

    Initialize Express server with basic route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log(`Server is ready for facebook Clone`);
+import express from "express";
+import ENV from "./utils/ENV.js";
+
+const app = express();
+
+app.get(`/`, (req, res) => {
+  res.send(`Facebook Clone is ready`);
+});
+
+const PORT = ENV.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT : ${PORT}`);
+});
---
+++ backend/utils/ENV.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const ENV = {
+  PORT: process.env.PORT,
+};
+
+export default ENV;
