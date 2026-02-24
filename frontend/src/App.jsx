3effb2b33d4b0781bf28612b0414957a366135c2
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-24 19:04:51 +0800

    Install all the dependencies and start to create simple route like Signup Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </>
   );
 };
 
